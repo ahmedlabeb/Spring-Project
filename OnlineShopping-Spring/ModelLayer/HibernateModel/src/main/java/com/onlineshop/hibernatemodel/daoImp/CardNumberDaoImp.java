330715d3984a33069a2e51c6aa13595c3febf79f
Nesmaa <Nesmaa@Nesmaa-PC>
2017-04-30 09:05:48 +0200

    "mycart"

--- OnlineShopping-Spring/ModelLayer/HibernateModel/src/main/java/com/onlineshop/hibernatemodel/daoImp/CardNumberDaoImp.java
+++ OnlineShopping-Spring/ModelLayer/HibernateModel/src/main/java/com/onlineshop/hibernatemodel/daoImp/CardNumberDaoImp.java
@@ -7,6 +7,10 @@ package com.onlineshop.hibernatemodel.daoImp;
 
 import com.onlineshop.hibernatemodel.daoInterface.CardNumberDaoInterface;
 import com.onlineshop.hibernatemodel.pojo.Rechargecards;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.orm.hibernate4.HibernateTemplate;
 
 /**
  *
@@ -14,24 +18,100 @@ import com.onlineshop.hibernatemodel.pojo.Rechargecards;
  */
 public class CardNumberDaoImp implements CardNumberDaoInterface{
     // Start Nesmaa
+   
+    TransactionTemplate transactiontemplate;
+
+    HibernateTemplate template;
+
+    public TransactionTemplate getTransactiontemplate() {
+        return transactiontemplate;
+    }
+
+    public void setTransactiontemplate(TransactionTemplate transactiontemplate) {
+        this.transactiontemplate = transactiontemplate;
+    }
+
+    public HibernateTemplate getTemplate() {
+        return template;
+    }
+
+    public void setTemplate(HibernateTemplate template) {
+        this.template = template;
+    }
+
+    // Start Nesmaa
     @Override
     public Rechargecards findCardNumber(int number) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        return (Rechargecards) transactiontemplate.execute(new TransactionCallback() {
+
+            @Override
+            public Rechargecards doInTransaction(TransactionStatus ts) {
+
+                Rechargecards rechargedCard = (Rechargecards) template.get(Rechargecards.class, number);
+
+                return rechargedCard;
+
+            }
+
+        });
+
     }
 
     @Override
     public Rechargecards insertCard(Rechargecards rechargecards) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        return (Rechargecards) transactiontemplate.execute(new TransactionCallback() {
+
+            @Override
+            public Rechargecards doInTransaction(TransactionStatus ts) {
+
+                template.save(rechargecards);
+
+                return rechargecards;
+
+            }
+
+        });
+
     }
 
     @Override
     public Rechargecards updateCardStatus(int rechargeCardID, String status) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        return (Rechargecards) transactiontemplate.execute(new TransactionCallback() {
+
+            @Override
+            public Rechargecards doInTransaction(TransactionStatus ts) {
+
+                Rechargecards rechargedCard = (Rechargecards) template.get(Rechargecards.class, rechargeCardID);
+                rechargedCard.setStatus(status);
+                template.save(rechargedCard);
+
+                return rechargedCard;
+
+            }
+
+        });
+
     }
 
     @Override
     public String findCardStatus(int rechargeCardID) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        return (String) transactiontemplate.execute(new TransactionCallback() {
+
+            @Override
+            public String doInTransaction(TransactionStatus ts) {
+
+                Rechargecards rechargedCard = (Rechargecards) template.get(Rechargecards.class, rechargeCardID);
+
+                return rechargedCard.getStatus();
+
+            }
+
+        });
+
     }
     // End Nesmaa
 }
--- OnlineShopping-Spring/ModelLayer/HibernateModel/src/test/java/test/main.java
+++ OnlineShopping-Spring/ModelLayer/HibernateModel/src/test/java/test/main.java
@@ -5,7 +5,9 @@
  */
 package test;
 
+import com.onlineshop.hibernatemodel.daoImp.CardNumberDaoImp;
 import com.onlineshop.hibernatemodel.daoImp.UserDaoImp;
+import com.onlineshop.hibernatemodel.pojo.Rechargecards;
 import com.onlineshop.hibernatemodel.pojo.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -18,16 +20,32 @@ public class main {
     public static void main(String[] args){
         ApplicationContext ctx = new ClassPathXmlApplicationContext("SpringConf.xml");
         //test shibo
-        UserDaoImp imp=(UserDaoImp) ctx.getBean("user");
+     //   UserDaoImp imp=(UserDaoImp) ctx.getBean("user");
         
-       User user= imp.selectUser("shaaban@gmail.com", "root");
-        System.out.println(""+user.getGender());
-        imp.insertUser(new User("shaaban@gmi.com", "male", "shibo", "ebrahim", "root", ""));
-        System.out.println("inserted successfuly");
-        for (User selectAllUser : imp.selectAllUsers()) {
-            System.out.println(""+selectAllUser.getEmail());
-        }
+      // User user= imp.selectUser("shaaban@gmail.com", "root");
+        //System.out.println(""+user.getGender());
+      //  imp.insertUser(new User("shaaban@gmi.com", "male", "shibo", "ebrahim", "root", ""));
+     //   System.out.println("inserted successfuly");
+     //   for (User selectAllUser : imp.selectAllUsers()) {
+           // System.out.println(""+selectAllUser.getEmail());
+       // }
         //test shiboo
+        
+       /* 
+        nesmaa
+        CardNumberDaoImp obj =(CardNumberDaoImp) ctx.getBean("cardnumber");
+        
+        //   obj.insertCard(new Rechargecards(1, 10, "0"));
+        //   System.out.println("insert");
+        //obj.updateCardStatus(0, "1");
+        //  Rechargecards findCardNumber = obj.findCardNumber(1);
+        //  System.out.println(findCardNumber.getStatus());
+        Rechargecards findCardNumber = obj.findCardNumber(0);
+        System.out.println(findCardNumber.getValue());
+        end nesma
+*/
+        
+     
         
     }
 }
